f65d7df0dba824e5c9c6b7f70de617959294a31b
하은지 <haeunji5072@gmail.com>
2021-12-15 00:04:32 +0900

    ✨ 구현 - 럭키스트레이트

--- 2. 구현/럭키스트레이트.py
+++ 2. 구현/럭키스트레이트.py
@@ -1,1 +1,13 @@
-# 풀 예정
+# 소요시간: 5분
+n = input()
+left = 0
+right = 0
+for i in range(0, len(n) // 2):
+    left += int(n[i])
+for i in range(len(n) // 2, len(n)):
+    right += int(n[i])
+
+if left == right:
+    print("LUCKY")
+else:
+    print("READY")
