6cf2dfee3f822c96bf6f5224a0122fffb25dddf2
하은지 <haeunji5072@gmail.com>
2022-03-03 17:37:13 +0900

    여행경로

--- 프로그래머스 Lv2/수식 최대화.py
+++ 프로그래머스 Lv2/수식 최대화.py
@@ -1,91 +1,94 @@
-from itertools import permutations
+# from itertools import permutations
 
-def calculate(elements, operates):  # operates == ('*', '-', '+')
-    # print("들어왔다: ", elements, operates)
-    all_elements = elements
-    result = 0
+# def calculate(elements, operates):  # operates == ('*', '-', '+')
+#     # print("들어왔다: ", elements, operates)
+#     all_elements = elements
+#     result = 0
     
-    for operate in operates:
-        while len(all_elements) != 1:
-            for i, element in enumerate(all_elements):
-                # print(i, operate, all_elements)
+#     for operate in operates:
+#         while len(all_elements) != 1:
+#             for i, element in enumerate(all_elements):
+#                 # print(i, operate, all_elements)
 
-                if element == operate:
-                    a = all_elements[i-1]
-                    b = all_elements[i+1]
-                    # print(a, b)
-                    if operate == "*":
-                        all_elements.insert(
-                            i+2, a * b)
-                    elif operate == "-":
-                        all_elements.insert(
-                            i+2, a - b)
-                    elif operate == "+":
-                        all_elements.insert(
-                            i+2, a + b)
+#                 if element == operate:
+#                     a = all_elements[i-1]
+#                     b = all_elements[i+1]
+#                     # print(a, b)
+#                     if operate == "*":
+#                         all_elements.insert(
+#                             i+2, a * b)
+#                     elif operate == "-":
+#                         all_elements.insert(
+#                             i+2, a - b)
+#                     elif operate == "+":
+#                         all_elements.insert(
+#                             i+2, a + b)
 
-                    # TODO: 빼줘야지
-                    # if len(all_elements) > 4:
-                    del all_elements[i-1]
-                    del all_elements[i-1]
-                    del all_elements[i-1]
-                    # print(all_elements)
-                    break
-            if operate not in all_elements:
-                break
-    # print(all_elements)
-    result = all_elements[0]
-    return result
+#                     # TODO: 빼줘야지
+#                     # if len(all_elements) > 4:
+#                     del all_elements[i-1]
+#                     del all_elements[i-1]
+#                     del all_elements[i-1]
+#                     # print(all_elements)
+#                     break
+#             if operate not in all_elements:
+#                 break
+#     # print(all_elements)
+#     result = all_elements[0]
+#     return result
 
 
-def solution(expression):
-    answer = 0
-    # 연산자 뽑기
-    # operates = []
-    # elements = []
-    # last_operate_index = -1
-    # for index, value in enumerate(expression):
-    #     if value == "-" or value == "*" or value == "+":
-    #         operates.append(value)
+# def solution(expression):
+#     answer = 0
+#     # 연산자 뽑기
+#     # operates = []
+#     # elements = []
+#     # last_operate_index = -1
+#     # for index, value in enumerate(expression):
+#     #     if value == "-" or value == "*" or value == "+":
+#     #         operates.append(value)
             
-    #         elements.append(int(expression[last_operate_index + 1:index])) 
-    #         elements.append(value)
+#     #         elements.append(int(expression[last_operate_index + 1:index])) 
+#     #         elements.append(value)
 
-    #         last_operate_index = index
-    #         # expression = expression[index+1:]
-    # elements.append(int(expression[last_operate_index + 1:]))
+#     #         last_operate_index = index
+#     #         # expression = expression[index+1:]
+#     # elements.append(int(expression[last_operate_index + 1:]))
 
-    # operates = list(set(operates))
-    # operate_combi = list(permutations(operates, len(operates)))
+#     # operates = list(set(operates))
+#     # operate_combi = list(permutations(operates, len(operates)))
 
 
-    operators = [x for x in ['+', '-', '*'] if x in expression]
-    operator_combi = list(permutations(operators, len(operators)))
-    # 계산해서 최댓값 뽑기
-    # print("elements입니다. ", elements)
-    candidates = [abs(calculate(elements[:], i)) for i in operator_combi]
-    # print("candidates ", candidates)
-    answer = max(candidates)
-    return answer
+#     operators = [x for x in ['+', '-', '*'] if x in expression]
+#     operator_combi = list(permutations(operators, len(operators)))
+#     # 계산해서 최댓값 뽑기
+#     # print("elements입니다. ", elements)
+#     candidates = [abs(calculate(elements[:], i)) for i in operator_combi]
+#     # print("candidates ", candidates)
+#     answer = max(candidates)
+#     return answer
 
+
+# print(solution("100-200*300-500+20"))
+
+# TODO: 1번째 풀이
+def solution(expression):
+    operations = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
+    answer = []
+    for op in operations:
+        a = op[0]
+        b = op[1]
+        temp_list = []
+        for e in expression.split(a):
+            temp = [f"({i})" for i in e.split(b)]
+            temp_list.append(f'({b.join(temp)})')
+        answer.append(abs(eval(a.join(temp_list))))
+    return max(answer)
 
 print(solution("100-200*300-500+20"))
 
-# TODO: 1번째 풀이
-# def solution(expression):
-#     operations = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
-#     answer = []
-#     for op in operations:
-#         a = op[0]
-#         b = op[1]
-#         temp_list = []
-#         for e in expression.split(a):
-#             temp = [f"({i})" for i in e.split(b)]
-#             temp_list.append(f'({b.join(temp)})')
-#         answer.append(abs(eval(a.join(temp_list))))
-#     return max(answer)
 
-
+# TODO: 2번째 풀이
 # import re
 # from itertools import permutations
 
---
+++ 프로그래머스 스쿨/여행경로.py
@@ -0,0 +1,38 @@
+def solution(tickets):
+    ticketsNum = len(tickets)
+    answer = []
+    caseStack = []
+    stack = []
+
+    lastCity = "ICN"
+    
+    while len(stack) != ticketsNum and tickets:
+        tempCase = []
+        for index, ticket in enumerate(tickets):
+          if ticket[0] == lastCity:
+            tempCase.append((len(stack), ticket))
+
+        if tempCase:
+          tempCase.sort()
+
+          nextCase = tempCase.pop(0)
+          stack.append(nextCase[1])
+          lastCity = nextCase[1][1]
+
+          tickets.remove(nextCase[1])
+
+          caseStack += tempCase[:]
+
+        else:
+          nextCase = caseStack.pop()
+          while len(stack) != nextCase[0]:
+            tickets.append(stack.pop())
+
+          stack.append(nextCase[1])
+          lastCity = nextCase[1][1]
+
+          tickets.remove(nextCase[1])
+
+    answer = [x[0] for x in stack]
+    answer.append(stack[-1][1])
+    return answer
