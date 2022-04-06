106f2ef59e44de5fbbf0aa79fefc9fe7400ee416
하은지 <haeunji5072@gmail.com>
2022-04-06 09:51:49 +0900

    1927

---
+++ 백준/1927.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+
+N = int(input())
+arr = []
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if not arr:
+            print(0)
+        else:
+            print(heapq.heappop(arr))
+    else:
+        heapq.heappush(arr, x)
