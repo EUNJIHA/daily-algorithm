0442b1cd9bdd578d30665d347f2cbb4dd0b4bb89
하은지 <haeunji5072@gmail.com>
2022-04-08 15:46:26 +0900

    boj 18870

---
+++ 백준/18870.py
@@ -0,0 +1,27 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+sortedArr = sorted(list(set(arr)))
+
+def binarySearch(arr, num):
+    answer = -1
+    # 정렬되어 있다고 가정
+    start, end = 0, len(arr)-1
+    # mid = len(arr) // 2
+
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == num:
+            return mid
+        elif arr[mid] < num:
+            start = mid + 1
+        elif arr[mid] > num:
+            end = mid - 1
+
+    return answer
+
+answer = []
+for num  in arr:
+    answer.append(str(binarySearch(sortedArr, num)))
+
+print(' '.join(answer))
