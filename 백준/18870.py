c0ca5b6b08863dbca9841de730ca4ffc87ef21ac
하은지 <haeunji5072@gmail.com>
2022-04-08 16:04:27 +0900

    boj 18870 다른 풀이 추가

--- 백준/18870.py
+++ 백준/18870.py
@@ -1,27 +1,36 @@
-N = int(input())
-arr = list(map(int, input().split()))
+# N = int(input())
+# arr = list(map(int, input().split()))
 
-sortedArr = sorted(list(set(arr)))
+# sortedArr = sorted(list(set(arr)))
 
-def binarySearch(arr, num):
-    answer = -1
-    # 정렬되어 있다고 가정
-    start, end = 0, len(arr)-1
-    # mid = len(arr) // 2
+# def binarySearch(arr, num):
+#     answer = -1
+#     # 정렬되어 있다고 가정
+#     start, end = 0, len(arr)-1
+#     # mid = len(arr) // 2
 
-    while start <= end:
-        mid = (start + end) // 2
-        if arr[mid] == num:
-            return mid
-        elif arr[mid] < num:
-            start = mid + 1
-        elif arr[mid] > num:
-            end = mid - 1
+#     while start <= end:
+#         mid = (start + end) // 2
+#         if arr[mid] == num:
+#             return mid
+#         elif arr[mid] < num:
+#             start = mid + 1
+#         elif arr[mid] > num:
+#             end = mid - 1
 
-    return answer
+#     return answer
 
-answer = []
-for num  in arr:
-    answer.append(str(binarySearch(sortedArr, num)))
+# answer = []
+# for num  in arr:
+#     answer.append(str(binarySearch(sortedArr, num)))
 
-print(' '.join(answer))
+# print(' '.join(answer))
+
+n = input()
+nums = list(map(int, input().split(" ")))
+
+arr = sorted(set((nums)))
+print(arr)
+num_dict = {n: i for i, n in enumerate(arr)}
+
+print(" ".join([str(num_dict[num]) for num in nums]))
