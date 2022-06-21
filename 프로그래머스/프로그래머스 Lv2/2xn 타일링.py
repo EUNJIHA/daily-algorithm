cdd3aa599a23c25c92adb53daa00e57eb817e153
하은지 <haeunji5072@gmail.com>
2022-06-21 22:20:05 +0900

    수정

--- 프로그래머스/프로그래머스 Lv2/2xn 타일링.py
+++ 프로그래머스/프로그래머스 Lv2/2xn 타일링.py
@@ -18,9 +18,8 @@ def solution(n):
     
     dp[1] = 1
     dp[2] = 2
-    dp[3] = 3
     
-    for i in range(4, n+1):
+    for i in range(3, n+1):
         dp[i] = (dp[i-1] + dp[i-2]) % 1000000007 # 분배법칙. 이를 이용하지 않으면 효율성 테스트 fail
         
     return dp[n]
